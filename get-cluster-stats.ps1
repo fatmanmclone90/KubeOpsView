abcdac05ed9be05f935b92df69d620646abf1d09
chris_tamlyn <Chris_Tamlyn@next.co.uk>
2023-06-08 11:42:15 +0100

    Adds script to output CPU allocations

---
+++ get-cluster-stats.ps1
@@ -0,0 +1,31 @@
+﻿function GetStringBetweenTwoStrings($text, $firstString, $secondString, $importPath){
+
+    $pattern = "(?s)$firstString((?s).*?)(?s)$secondString"
+    # munging the formatting of multiline strings
+    $result = [regex]::Match($text,$pattern).Groups[1].Value
+
+    return $result
+}
+
+$nodes = kubectl get nodes -o NAME
+
+# Repeat for memory
+$cpus = @();
+
+# formatting wrong
+$cpus += "Request`tLimit"
+$cpus += "---`t---"
+
+foreach ($node in $nodes)
+{
+    $node = $node.TrimStart("node/")
+    $describe = kubectl describe node $node   
+    $resources = GetStringBetweenTwoStrings $describe "Allocated resources:" "Events:"
+    #$resources = $resources.Trim()
+    #$resources = $resources.TrimStart("(Total limits may be over 100 percent, i.e., overcommitted.)")
+
+    $cpu = GetStringBetweenTwoStrings $resources "cpu" "memory"
+    $cpus += $cpu.Trim();
+}
+
+Write-Output $cpus
